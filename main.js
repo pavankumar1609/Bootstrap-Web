21106ae4a91408338976409650c4a182d71fcc9d
Pavan Kumar <pavankumarmaddala585@gmail.com>
2022-01-08 17:45:22 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicGF2YW5rdW1hcjE2MDkiLCJhIjoiY2t4aXp5eWFvMGZqNzJ4cWsxaHhnMnB3bSJ9.qDzVWRFYFqMEBXOFlyL-PA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zoom: 18,
+});
