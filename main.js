df5e81794e7fbefe1da90c97e99553893cd3be37
Pavan Kumar <pavankumarmaddala585@gmail.com>
2022-01-22 12:05:39 +0530

    Change location on mapbox

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 mapboxgl.accessToken =
-  "pk.eyJ1IjoicGF2YW5rdW1hcjE2MDkiLCJhIjoiY2t4aXp5eWFvMGZqNzJ4cWsxaHhnMnB3bSJ9.qDzVWRFYFqMEBXOFlyL-PA";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [-71.060982, 42.35725],
-  zoom: 18,
+  "pk.eyJ1IjoicGF2YW5rdW1hcjE2MzAiLCJhIjoiY2t5cGVqdGNkMDk4NjJwcHRkdDA4ZzB6aiJ9.24cOIGDA7nge92cVhVZKzA";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
 });
+
+function successLocation(position) {
+  setMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setMap([83.896782, 18.296974]);
+}
+
+function setMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 10,
+  });
+  map.addControl(new mapboxgl.NavigationControl());
+}
